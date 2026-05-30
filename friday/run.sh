da5ef770590336d339e59daab2ce6a0b04d13f22
johnleonard1725-cpu <kellerleonard79@gmail.com>
2026-05-30 16:17:15 -0500

    New Path for Friday

--- friday/run.sh
+++ friday/run.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 source ~/.zshrc
-exec /Library/Frameworks/Python.framework/Versions/3.14/bin/python3 /Users/keller/Desktop/friday/friday/friday.py
+exec /Library/Frameworks/Python.framework/Versions/3.14/bin/python3 /Users/keller/friday/friday/friday.py
